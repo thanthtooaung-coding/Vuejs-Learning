4ccabf39794ae58b42a707b720dd2a13104e72bd
Vinnnm <thanthtoo1285@gmail.com>
2024-08-28 16:42:25 +0630

    vue-start-spa Day(1)

---
+++ vue-start-spa/src/components/PageViewer.vue
@@ -0,0 +1,28 @@
+<template>
+    <div id="content" class="container">
+        <h1 class="emphasize"> {{ page.pageTitle }} </h1>
+        <p> {{ page.content }} </p>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        page: {
+            type: Object,
+            default(rawPropes) {
+                return {
+                    pageTitle: '',
+                    content: ''
+                };
+            }
+        }
+    }
+};
+</script>
+
+<style scoped>
+.emphasize {
+    color: blue;
+}
+</style>
